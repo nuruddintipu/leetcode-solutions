4f35a67eaf2282040c059016fde352a460e0a650
Nuruddin Tipu <nuruddintipu.connect@gmail.com>
2025-06-14 16:24:13 +0600

    refactor: update ListNode to use public access modifiers for fields and constructors

--- src/com/leetcode/common/ListNode.java
+++ src/com/leetcode/common/ListNode.java
@@ -1,14 +1,14 @@
 package com.leetcode.common;
 
 public class ListNode {
-    int val;
-    ListNode next;
+    public int val;
+    public ListNode next;
 
-    ListNode(int val) {
+    public ListNode(int val) {
         this.val = val;
     }
 
-    ListNode(int val, ListNode next){
+    public ListNode(int val, ListNode next){
         this.val = val;
         this.next = next;
     }
